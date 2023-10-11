5e4a07b96a1bfed59d6bdb9be845135f788679bd
JosueDeLosSantos <jdd1993@hotmail.com>
2023-10-11 07:25:50 -0400

    Add responsive styles to the hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,28 +1,13 @@
 import { Button, Container, Typography } from "@mui/material"
 
-const containerStyles = {
-    marginTop: "15vh",
-	display: "flex",
-	flexDirection: "column",
-    alignItems: "center",
-    minWidth: "80%",
-}
-
-const typographyStyles = {
-	textAlign: "center",
-	fontSize: "3rem",
-    fontFamily: "PoiretOne",
-    marginBottom: "5rem",
-}
-
 export default function Hero({handlePlaying}) {
 	return (
-		<Container style={containerStyles} maxWidth="sm">
-			<Typography variant="h1" component="h2" style={typographyStyles}>
+		<Container className="containerStyles" maxWidth="sm">
+			<Typography className="typographyHeroStyles" variant="h1" component="h2" /* style={typographyStyles} */>
 				Training your photographic memory has never been so fun!
 			</Typography>
-			<Button onClick={handlePlaying} variant="contained">
-				<Typography fontSize="1.5rem" variant="button" component="h3">Play</Typography>
+			<Button className="playBtn" onClick={handlePlaying} variant="contained">
+				<Typography className="playBtnTypo" variant="button" component="h3">Play</Typography>
 			</Button>
 		</Container>
 	)
